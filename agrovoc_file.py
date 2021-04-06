59c13d7081392a30000b0c97754a80fb34bccf03
Ing David <46038020+Ing-David@users.noreply.github.com>
2021-04-06 17:14:46 +0200

    Update agrovoc_file.py

--- agrovoc_file.py
+++ agrovoc_file.py
@@ -55,7 +55,7 @@ def agrovoc_types():
 def agrovoc_type_instance():
     qres = g.query("""PREFIX skos: <http://www.w3.org/2004/02/skos/core#> 
                       PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#> 
-                      SELECT ?node ?type WHERE {?node a ?type.} Limit 10""")
+                      SELECT ?node ?type WHERE {?node a ?type.}""")
     column_names = ["node","type"]
     df_type_instance = pd.DataFrame([row for row in qres], columns = column_names)
     df_type_instance.to_csv("agrovoc-type-instance.tsv",sep="\t", index=False, header=None) 
