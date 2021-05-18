8e5ac80ecc47c604f63650d62e3afad5c2c805f5
Andon Tchechmedjiev <andon.tchechmedjiev@mines-ales.fr>
2021-05-18 11:26:42 +0200

    Updated glove conversion script to ignore lines that are too long

--- glove.py
+++ glove.py
@@ -1,10 +1,18 @@
 import numpy as np
+
+embeddings_dict = {}
+with open("glove840b300d.txt", 'r') as f:
+    for line in f:
+        values = line.strip().split(' ')
+        word = values[0]
+        vector = np.asarray(values[1:], "float32")
+        embeddings_dict[word] = vector 
+
+
+words =  list(embeddings_dict.keys())
+vectors = np.array([embeddings_dict[word] for word in words])
+
  
-glove = np.loadtxt("glove840b300d.txt", dtype='str', comments=None)
-words = glove[:, 0]
-vectors = glove[:, 1:].astype('float')
- 
-words=words.tolist()
 with open('glove.word', 'w') as filehandle:
     filehandle.writelines("%s\n" % word for word in words)
  
